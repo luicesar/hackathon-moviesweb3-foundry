830d8c2c1150aa831a9a462b0a79136159f6bacc
Luis Oliveira <luis.oliveira@thomasgregit.com.br>
2024-03-23 10:20:53 -0300

    add arquivo de deploy e docker

--- smartcontracts/script/deploy.local.s.sol
+++ smartcontracts/script/deploy.local.s.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "src/Counter.sol";
+import {Counter} from "../src/Counter.sol";
 
-contract CounterScript is Script {
+contract Local is Script {
 
     function setUp() public {}
 
